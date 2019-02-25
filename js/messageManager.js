59e600d7a5516e4cea5c81ad4d4a0fd2efc06639
Cameron Stevenson <cameron.stevenson03@gmail.com>
2019-02-25 17:57:22 +1300

    Basic room support

--- js/messageManager.js
+++ js/messageManager.js
@@ -3,8 +3,12 @@
 let noFileString = "nofile";
 
 let inputTextFieldId = "input_text_field";
+let roomNameTextFieldId = "room_switch_text_area";
 
-let testRoom = "abc"; // magic number
+let testRoom = "welcome"; // magic number
+let roomPassword = "";
+
+let getMessageMutex = 1;
 
 window.onload = init;
 
@@ -12,17 +16,82 @@ function init(){
     //css changes
     document.getElementById("input").style.backgroundColor = "grey";
     document.getElementById(inputTextFieldId).addEventListener("keypress", keyupOnInputTextField);
+    document.getElementById(roomNameTextFieldId).addEventListener("keypress", keyupOnRoomNameTextField);
+    document.getElementById("room_create_enter").addEventListener("click", clickOnRoomCreateEnter);
     //init get message loop
     getMessage(testRoom, nextMessage);
 }
 
+function createRoom(name, accessByURL, accessByPassword, password){
+    getMessageMutex = 0;
+    fetch(noFileString, 
+    {method: "POST", 
+    body: "", 
+    headers: {
+    "Content-Type": "text/plain",
+    "Command":"createRoom",
+    "Room":name,
+    "Room-Access-By-URL":accessByURL,
+    "Room-Access-By-Password":accessByPassword,
+    "Room-Password":password
+    }
+    }
+    ).then(createMessageResponse);
+}
+
+function createMessageResponse(response){
+    if(response.status==200){
+        console.log("yes");
+    }
+    switchRoom(response.headers.get("Room"));
+    getMessageMutex = 1;
+}
+
+function clickOnRoomCreateEnter(e){
+    let name = document.getElementById("room_create_name").value;
+    let password = document.getElementById("room_create_password").value;
+    let accessByURL = document.getElementById("access_by_url").checked; 
+    let accessByPassword = document.getElementById("access_by_password").checked; 
+    roomPassword = password;
+    createRoom(name,accessByURL,accessByPassword,password);
+}
+
+function switchRoom(roomName){
+    
+    var node = document.getElementById("messages_container2");
+    testRoom = roomName;
+    //roomPassword = document.getElementById("room_switch_password").value;
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+    nextMessage = 0;
+    document.getElementById(roomNameTextFieldId).value = roomName;
+}
+
+function enterOnRoomName(e){
+    e.preventDefault();
+    roomName = document.getElementById(roomNameTextFieldId).value;
+    switchRoom(roomName);
+    roomPassword = document.getElementById("room_switch_password").value;
+}
+
+function keyupOnRoomNameTextField(e){
+    if (e.keyCode == 13){
+        enterOnRoomName(e);
+    }
+}
+
+function enterOnInput(e){
+    e.preventDefault();
+    message = document.getElementById(inputTextFieldId).value;
+    console.log(message);
+    document.getElementById(inputTextFieldId).value = "";
+    createMessage(testRoom, message);
+}
+
 function keyupOnInputTextField (e){
     if (e.keyCode == 13){
-        e.preventDefault();
-        message = document.getElementById(inputTextFieldId).value;
-        console.log(message);
-        document.getElementById(inputTextFieldId).value = "";
-        createMessage(testRoom, message);
+        enterOnInput(e);
     }
 }
 
@@ -34,7 +103,8 @@ function createMessage(room, message){
     headers: {
     "Content-Type": "text/plain",
     "Command":"createMessage",
-    "Room":room
+    "Room":room,
+    "Room-Password":roomPassword
     }
     }
     ).then(postMessageResponse);
@@ -47,6 +117,11 @@ function postMessageResponse(response){
 let nextMessage = 0; //the next message which needs to be loaded
 
 function getMessage(room, messageIndex){
+    console.log(roomPassword);
+    if(getMessageMutex == 0){
+        sleep(50).then(() => {getMessage(testRoom, nextMessage);});
+        return;
+    }
     //fetch message
     fetch(noFileString, 
     {method: "POST",
@@ -54,7 +129,8 @@ function getMessage(room, messageIndex){
     "Content-Type": "text/plain",
     "Command": "getMessage",
     "Room": room,
-    "Message-Index":messageIndex
+    "Message-Index":messageIndex,
+    "Room-Password":roomPassword
     }
     }
     ).then(getMessageResponse);
